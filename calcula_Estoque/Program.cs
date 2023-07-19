a6149badf12d1ecb900f0f9cafbfda05b44ac4da
Vinicius Lobato <Vinicius.lobatz@gmail.com>
2023-07-18 22:21:41 -0300

    1 - Insert calcula_estoque
    Calcula média estoque

---
+++ calcula_Estoque/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class calcula_Estoque
+{
+    static void Main()
+    {
+
+        // Declarando as variáveis
+        double minimo = 0, maximo = 0;
+
+        Console.WriteLine("Média de Estoque C#\r");
+        Console.WriteLine("------------------------\n");
+
+        // Recebendo os parametros do usuário
+        Console.Write("\nDigite o minimo do estoque: ");
+        minimo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("\nDigite o maximo do estoque: ");
+        maximo = Convert.ToDouble(Console.ReadLine());
+
+        // Saida para o usuário
+        Console.WriteLine("------------------------\n");
+        Console.WriteLine($"\nMédia do estoque:{(minimo + maximo) / 2}");
+
+        // Fechando a aplicação
+        Console.WriteLine("------------------------\n");
+        Console.WriteLine($"\nPressione qualquer tecla para sair");
+        Console.ReadLine();
+
+    }
+}
