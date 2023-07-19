29d8a3eed376dc844160dbfbca83b9b256c850ba
Vinicius Lobato <Vinicius.lobatz@gmail.com>
2023-07-19 00:11:17 -0300

    1 - Controle de estoque

--- calcula_Estoque/Program.cs
+++ calcula_Estoque/Program.cs
@@ -4,23 +4,23 @@ class calcula_Estoque
     static void Main()
     {
 
-        // Declarando as variáveis
+        //Declarando as variáveis
         double minimo = 0, maximo = 0;
 
         Console.WriteLine("Média de Estoque C#\r");
         Console.WriteLine("------------------------\n");
 
-        // Recebendo os parametros do usuário
+        //Recebendo os parametros do usuário
         Console.Write("\nDigite o minimo do estoque: ");
         minimo = Convert.ToDouble(Console.ReadLine());
         Console.Write("\nDigite o maximo do estoque: ");
         maximo = Convert.ToDouble(Console.ReadLine());
 
-        // Saida para o usuário
+        //Saida para o usuário
         Console.WriteLine("------------------------\n");
         Console.WriteLine($"\nMédia do estoque:{(minimo + maximo) / 2}");
 
-        // Fechando a aplicação
+        //Fechando a aplicação
         Console.WriteLine("------------------------\n");
         Console.WriteLine($"\nPressione qualquer tecla para sair");
         Console.ReadLine();
